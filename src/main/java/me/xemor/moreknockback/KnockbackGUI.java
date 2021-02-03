b8eade45cfb6a582a9f5b7e0d0040e8fa84554b2
Xemorr <31805746+Xemorr@users.noreply.github.com>
2021-02-03 19:54:21 +0000

    first commit

---
+++ src/main/java/me/xemor/moreknockback/KnockbackGUI.java
@@ -0,0 +1,13 @@
+package me.xemor.moreknockback;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.AbstractGui;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class KnockbackGUI extends AbstractGui {
+
+
+
+}
